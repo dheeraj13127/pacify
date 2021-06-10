cd0f4bcc4d100b97c079f2052d9fdc70a559d6dc
dheeraj13127 <67220444+dheeraj13127@users.noreply.github.com>
2021-06-10 20:35:59 +0530

    Update BigLogo.js

--- src/components/LandingComponents/BigLogo.js
+++ src/components/LandingComponents/BigLogo.js
@@ -8,7 +8,7 @@ function BigLogo() {
          <h4 style={{fontWeight:"300"}}>Healthy life with</h4>
          <h3 style={{fontWeight:"bold"}}>Nature Organic</h3>
          <p style={{fontWeight:"500"}}>Vegetables are the edible parts of the plant<br/>that is used in cooking or can be eaten now</p>
-         <button className="btn waves-effect bigLogoButton">Explore Now</button>
+         <button className="btn waves-effect bigLogoButton">Explore  Now</button>
         </div>
         <div className="col s12 l7 m7">
           <div className="center-align">
